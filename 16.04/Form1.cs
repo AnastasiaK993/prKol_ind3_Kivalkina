4791256a25dae1bef55f48ba5feb572834db4dda
AnastasiaK993 <akivalkina@list.ru>
2025-04-16 15:35:35 +0500

    Add files via upload

---
+++ 16.04/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16._04
+{
+    public partial class Form1 : Form
+    {
+        ArrayList pochts = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text= pochts
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pochta pochta = new Pochta(textBox1.Text, textBox2.Text, textBox3.Text);
+            pochts.Add(pochta);
+            listBox1.Items.Add(pochta.NameOrganoz);
+        }
+    }
+}
---
+++ 16.04/Pochta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._04
+{
+    internal class Pochta
+    {
+        string nameOrganoz;
+
+        public Pochta(string NameOrganoz_, string Street_, string Numberhome_)
+        {
+            nameOrganoz = NameOrganoz_;
+            street = Street_;
+            numberhome = Numberhome_;
+        }
+        public string NameOrganoz
+        {
+            get;
+            set;
+        }
+        string street;
+        public string Street
+        {
+            get;
+            set;
+        }
+        string numberhome;
+        public string Numberhome
+        {
+            get;
+            set;
+        }
+
+    }
+}
